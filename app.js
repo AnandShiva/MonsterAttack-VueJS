fdb1ee11a890678dd86c18231da7e37e6af30949
Anandkumar S <anandkumar.s@sap.com>
2019-09-06 15:44:39 +0530

    Scrollable logs container - Functional completion

--- app.js
+++ app.js
@@ -1,110 +1,103 @@
 let viewInstance = new Vue({
-    el : "#vue_container",
-    data : {
-        title : "Monster Madness",
-        bGameInProgress : false,
-        yourHealth : 100,
-        monsterHealth : 100,
-        logs : [{
-            won : 'you',
-            move : 'attack',
-            stepStatus : 'You Attacked. Monster Damage - 12. Your Damage - 10.'
-        },
-        {
-            won : 'monster',
-            move : 'heal',
-            stepStatus : 'You Healed. Monster Damage - 0. Your Damage - 10.'
-        }]
+    el: "#vue_container",
+    data: {
+        title: "Monster Madness",
+        bGameInProgress: false,
+        yourHealth: 100,
+        monsterHealth: 100,
+        logs: []
     },
-    methods :{
-        start_game : function(){
+    methods: {
+        start_game: function () {
             this.bGameInProgress = true;
             this.yourHealth = 100;
             this.monsterHealth = 100;
             this.logs = [];
-        }, 
-        end_game : function(){
+        },
+        end_game: function () {
             this.bGameInProgress = false;
             this.logs = [];
-            if(this.yourHealth > this.monsterHealth){
+            if (this.yourHealth > this.monsterHealth) {
                 this.logs.push({
-                    won : 'you',
-                    stepStatus : 'You Won !!'
+                    won: 'you',
+                    stepStatus: 'You Won !!'
                 });
-            }else{
+            } else {
                 this.logs.push({
-                    won : 'monster',
-                    stepStatus : 'Monster Won :\'( '
+                    won: 'monster',
+                    stepStatus: 'Monster Won :\'( '
                 });
             }
 
-        }, 
-        attack : function(){
-            let yourAttackPower = Math.random()*10;
-            let monsterAttackPower = Math.random()*10;
+        },
+        attack: function () {
+            let yourAttackPower =  Math.floor(Math.random() * 10);
+            let monsterAttackPower =  Math.floor(Math.random() * 10);
             this.yourHealth = Math.floor(this.yourHealth - monsterAttackPower);
             this.monsterHealth = Math.floor(this.monsterHealth - yourAttackPower);
-            if(this.yourHealth <= 0){
+            this.logs.push({
+                won: yourAttackPower > monsterAttackPower ? 'you' : 'monster',
+                stepStatus: 'You Attacked !!' + 'Your Attack Power : ' + yourAttackPower + '. Monster Attack Power : ' + monsterAttackPower + "."
+            });
+
+            if (this.yourHealth <= 0) {
                 this.bGameInProgress = false;
                 this.logs.push({
-                    won : 'monster',
-                    stepStatus : 'Monster Won :\'( '
+                    won: 'monster',
+                    stepStatus: 'Monster Won :\'( '
+                });
+            }
+            if (this.monsterHealth <= 0) {
+                this.bGameInProgress = false;
+                this.logs.push({
+                    won: 'you',
+                    stepStatus: 'You  Won :-D !!!'
                 });
             }
 
-            if(this.monsterHealth <= 0){
-                this.bGameInProgress = false;
-                this.logs.push({
-                    won : 'you',
-                    stepStatus : 'You  Won :-D !!!'
-                });
-            }
-
-        }, 
-        heal : function(){
-            let yourHealPower = Math.random()*10;
-            let monsterAttackPower = Math.random()*10;
+        },
+        heal: function () {
+            let yourHealPower = Math.floor(Math.random() * 10);
+            let monsterAttackPower =  Math.floor(Math.random() * 10);
             this.yourHealth = Math.floor(this.yourHealth - monsterAttackPower + yourHealPower);
-            if(this.yourHealth <= 0){
+            this.logs.push({
+                won: yourHealPower > monsterAttackPower ? 'you' : 'monster',
+                stepStatus: 'You healed :) ' + 'Your Heal Power : ' + yourHealPower + '. Monster Attack Power : ' + monsterAttackPower + "."
+            });
+            if (this.yourHealth <= 0) {
                 this.bGameInProgress = false;
                 this.logs.push({
-                    won : 'monster',
-                    stepStatus : 'Monster Won :\'( '
+                    won: 'monster',
+                    stepStatus: 'Monster Won :\'( '
                 });
             }
-
-            if(this.monsterHealth <= 0){
+            if (this.monsterHealth <= 0) {
                 this.bGameInProgress = false;
                 this.logs.push({
-                    won : 'you',
-                    stepStatus : 'You  Won :-D !!!'
+                    won: 'you',
+                    stepStatus: 'You  Won :-D !!!'
                 });
             }
-
-
-        }, 
-        special_attack : function(){
-            let yourAttackPower = Math.random()*20;
-            let monsterAttackPower = Math.random()*20;
+        },
+        special_attack: function () {
+            let yourAttackPower = Math.random() * 20;
+            let monsterAttackPower = Math.random() * 20;
             this.yourHealth = Math.floor(this.yourHealth - monsterAttackPower);
             this.monsterHealth = Math.floor(this.monsterHealth - yourAttackPower);
-            if(this.yourHealth <= 0){
+            if (this.yourHealth <= 0) {
                 this.bGameInProgress = false;
                 this.logs.push({
-                    won : 'monster',
-                    stepStatus : 'Monster Won :\'( '
+                    won: 'monster',
+                    stepStatus: 'Monster Won :\'( '
                 });
             }
-
-            if(this.monsterHealth <= 0){
+            if (this.monsterHealth <= 0) {
                 this.bGameInProgress = false;
                 this.logs.push({
-                    won : 'you',
-                    stepStatus : 'You  Won :-D !!!'
+                    won: 'you',
+                    stepStatus: 'You  Won :-D !!!'
                 });
             }
-
-
         }
     }
 })
